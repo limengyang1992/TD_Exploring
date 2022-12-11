84b15ef980545c65c0c872ab1bd30f063d6581b9
Lishjie <851275924@qq.com>
2022-12-11 22:35:26 +0800

    main

--- run.sh
+++ run.sh
@@ -4,20 +4,28 @@
 epochs=200
 
 (
-python tools/train.py --model convnext_tiny --dataset cifar10  --epochs ${epochs}  --optims adamw --lr 0.01  && \
-python tools/train.py --model convnext_tiny --dataset cifar100  --epochs ${epochs}   --optims adamw --lr 0.01  && \
 
-python tools/train.py --model swin_t --dataset cifar10 --epochs ${epochs}  --sched cosine --lr 0.01 && \
-python tools/train.py --model swin_t --dataset cifar100 --epochs ${epochs}  --sched cosine --lr 0.01 && \
 
-python tools/train.py --model vgg16 --dataset cifar10 --epochs ${epochs}   && \
-python tools/train.py --model vgg16 --dataset cifar100 --epochs ${epochs}   && \
+python tools/train.py --model ResNet50 --dataset cifar10 --epochs ${epochs}   && \
+python tools/train.py --model ResNet50 --dataset cifar100 --epochs ${epochs}  && \
 
-python tools/train.py --model mobilenetv2 --dataset cifar10 --epochs ${epochs}   && \
-python tools/train.py --model mobilenetv2 --dataset cifar100 --epochs ${epochs}   && \
 
-python tools/train.py --model resnet32 --dataset cifar10 --epochs ${epochs}   && \
-python tools/train.py --model resnet32 --dataset cifar100 --epochs ${epochs}    \
+python tools/train.py --model wideresnet40_10 --dataset cifar10 --epochs ${epochs}   && \
+python tools/train.py --model wideresnet40_10 --dataset cifar100 --epochs ${epochs}  \
+
+
+# python tools/train.py --model mobilenetv2 --dataset cifar10 --epochs ${epochs}   && \
+# python tools/train.py --model mobilenetv2 --dataset cifar100 --epochs ${epochs}   && \
+
+# python tools/train.py --model vgg16 --dataset cifar10 --epochs ${epochs}   && \
+# python tools/train.py --model vgg16 --dataset cifar100 --epochs ${epochs}   && \
+
+# python tools/train.py --model swin_t --dataset cifar100 --epochs ${epochs}  --sched cosine --lr 0.01 && \
+# python tools/train.py --model swin_t --dataset cifar10 --epochs ${epochs}  --sched cosine --lr 0.01 && \
+
+# python tools/train.py --model convnext_tiny --dataset cifar10  --epochs ${epochs}  --optims adamw --lr 0.01  && \
+# python tools/train.py --model convnext_tiny --dataset cifar100  --epochs ${epochs}   --optims adamw --lr 0.01  \
+
 
 ) &
 
